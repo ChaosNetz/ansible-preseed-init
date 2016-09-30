85f5d2eddb0a71be2a141bff26541106ea92bd93
yanosz <yanosz@gmx.net>
2016-09-30 18:57:26 +0200

    Added vim Following flueschmiede upstream

--- roles/c4-inventur/files/inventur.sh
+++ roles/c4-inventur/files/inventur.sh
@@ -3,5 +3,5 @@ fn="`date +%s`"
 lshw > /tmp/$fn
 sn="`cat /tmp/$fn | egrep -oi 'serial:.*' | head -n 1 | cut -d\  --fields=2`"
 mv /tmp/$fn /tmp/$sn-$fn
-curl -u ftp:ftp -T /tmp/$sn-$fn ftp://weltarchiv.labor.koeln.ccc.de/incoming/inventory/
+curl -u ftp:ftp -T /tmp/$sn-$fn ftp://trillian.labor.koeln.ccc.de/incoming/inventory/
 
