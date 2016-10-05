16ed53c29bae4faa2b5f9bd38043d2ea050ecc2c
Kai Oliver Quambusch <kq@qsc.de>
2016-10-05 23:18:00 +0200

    c4-inventur/script: fixed file not found (added copy task & executing from /usr/local/bin)

--- roles/c4-inventur/files/inventur.sh
+++ roles/c4-inventur/files/inventur.sh
@@ -3,7 +3,7 @@ export LC_ALL=C
 
 fn="`date +%s`"
 lshw -json > /tmp/$fn
-sn="`python2 serial.py /tmp/$fn`"
+sn="`python2 /usr/local/bin/serial.py /tmp/$fn`"
 mv /tmp/$fn /tmp/$sn-$fn
 curl -u ftp:ftp -T /tmp/$sn-$fn ftp://trillian.labor.koeln.ccc.de/incoming/inventory/
 
