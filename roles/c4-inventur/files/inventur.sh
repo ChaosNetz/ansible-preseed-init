c90f9df536eba1b4e5a63570f9a9a1c48e12767d
Kai Oliver Quambusch <kq@qsc.de>
2016-10-05 22:35:38 +0200

    c4-inventur/script: retrieve serial from json

--- roles/c4-inventur/files/inventur.sh
+++ roles/c4-inventur/files/inventur.sh
@@ -3,7 +3,7 @@ export LC_ALL=C
 
 fn="`date +%s`"
 lshw -json > /tmp/$fn
-sn="`cat /tmp/$fn | egrep -oi 'serial:.*' | head -n 1 | cut -d\  --fields=2`"
+sn="`python2 serial.py /tmp/$fn`"
 mv /tmp/$fn /tmp/$sn-$fn
-curl -u ftp:ftp -T /tmp/$sn-$fn ftp://trillian.labor.koeln.ccc.de/incoming/inventory/
+#curl -u ftp:ftp -T /tmp/$sn-$fn ftp://trillian.labor.koeln.ccc.de/incoming/inventory/
 
