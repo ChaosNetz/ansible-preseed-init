4b090ecd335ad49b7a2d5e70218cd26f3c086d18
Kai Oliver Quambusch <kq@qsc.de>
2016-10-05 22:36:17 +0200

    c4-inventur/script: removed comment line

--- roles/c4-inventur/files/inventur.sh
+++ roles/c4-inventur/files/inventur.sh
@@ -5,5 +5,5 @@ fn="`date +%s`"
 lshw -json > /tmp/$fn
 sn="`python2 serial.py /tmp/$fn`"
 mv /tmp/$fn /tmp/$sn-$fn
-#curl -u ftp:ftp -T /tmp/$sn-$fn ftp://trillian.labor.koeln.ccc.de/incoming/inventory/
+curl -u ftp:ftp -T /tmp/$sn-$fn ftp://trillian.labor.koeln.ccc.de/incoming/inventory/
 
