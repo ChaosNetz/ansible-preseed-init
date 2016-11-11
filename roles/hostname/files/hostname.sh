a610b70a129db5ba53a7194245381dad2249254c
Kai Oliver Quambusch <kq@qsc.de>
2016-11-11 11:03:53 +0100

    modified IP, now points to web service running on http://icvpn1.kbu.freifunk.net port 8989

--- roles/hostname/files/hostname.sh
+++ roles/hostname/files/hostname.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 MAC=$(ip addr show eth0  | egrep -oi 'ether ([a-f0-9]{2}:){5}[a-f0-9]{2}' | cut --delimiter=' ' -f2 | tr ':' '-')
-HOSTNAME=`curl -sLi "http://127.0.0.1:8989/pxe/hostname/${MAC}" 2> /dev/zero | tail -n1`
+HOSTNAME=`curl -sLi "http://icvpn1.kbu.freifunk.net:8989/pxe/hostname/${MAC}" 2> /dev/zero | tail -n1`
 rm -f /etc/hostname && touch /etc/hostname && echo "$HOSTNAME" >> /etc/hostname
