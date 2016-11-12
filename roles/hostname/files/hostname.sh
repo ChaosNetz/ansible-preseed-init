b695ea6b7e8b929d191946349d3116e5d9a45177
Kai Oliver Quambusch <kq@qsc.de>
2016-11-12 01:15:38 +0100

    fixed network interface mismatch (eth0 isn't available on intel i5 dell latitudes..)

--- roles/hostname/files/hostname.sh
+++ roles/hostname/files/hostname.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-MAC=$(ip addr show eth0  | egrep -oi 'ether ([a-f0-9]{2}:){5}[a-f0-9]{2}' | cut --delimiter=' ' -f2 | tr ':' '-')
+MAC=$(ip addr show eth0 2> /dev/null || ip addr show eno1 | egrep -oi 'ether ([a-f0-9]{2}:){5}[a-f0-9]{2}' | cut --delimiter=' ' -f2 | tr ':' '-')
 HOSTNAME=`curl -sLi "http://212.201.68.131:8989/pxe/hostname/${MAC}" 2> /dev/zero | tail -n1`
 rm -f /etc/hostname && touch /etc/hostname && echo "$HOSTNAME" >> /etc/hostname
