0caffb0f1ca44d3bef8b57782d74ac15dfdf61bd
yanosz <yanosz@gmx.net>
2016-11-19 17:58:01 +0100

    Use cacert (until we've let's encrypt)

--- roles/hostname/files/hostname.sh
+++ roles/hostname/files/hostname.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 MAC=$(ip link show 2> /dev/null | tail -n 1 |  egrep -oi 'ether ([a-f0-9]{2}:){5}[a-f0-9]{2}' | cut --delimiter=' ' -f2 | tr ':' '-')
-HOSTNAME=`curl -sLik "https://icvpn1.chaosnetz.org:8989/pxe/hostname/${MAC}" 2> /dev/zero | tail -n1`
+HOSTNAME=`curl --cacert /usr/share/ca-certificates/CAcert/root.crt -sLi "https://icvpn1.chaosnetz.org:8989/pxe/hostname/${MAC}" 2> /dev/zero | tail -n1`
 rm -f /etc/hostname && touch /etc/hostname && echo "$HOSTNAME" >> /etc/hostname
 /etc/init.d/hostname.sh
