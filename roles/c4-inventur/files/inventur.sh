c601c6baefaff1c800aa8ad3114e5ed3293c2d2e
Kai Oliver Quambusch <kq@qsc.de>
2016-11-19 19:08:41 +0100

    fixed

--- roles/c4-inventur/files/inventur.sh
+++ roles/c4-inventur/files/inventur.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 export LC_ALL=C
 
-curl -sLi -X "POST" --cacert "/usr/share/ca-certificates/CAcert/root.crt" -F "lshw=`lshw -json`" "https://icvpn1.chaosnetz.org:8989/pxe/lshw/$(hostname)"
+curl -sLi -X "POST" --cacert "/usr/share/ca-certificates/CAcert/root.crt" -F "lshw=@-" "https://icvpn1.chaosnetz.org:8989/pxe/lshw/$(hostname)" << CURL_DATA
+`lshw -json`
+CURL_DATA
