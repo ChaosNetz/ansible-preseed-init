fe3b3c4d7b18de1cd8f2160b5814eee56f0035f0
yanosz <yanosz@gmx.net>
2016-11-19 22:13:16 +0100

    Set hostname via ansible

--- roles/hostname/files/hostname.sh
+++ roles/hostname/files/hostname.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 MAC=$(ip link show 2> /dev/null | tail -n 1 |  egrep -oi 'ether ([a-f0-9]{2}:){5}[a-f0-9]{2}' | cut --delimiter=' ' -f2 | tr ':' '-')
-HOSTNAME=`curl --cacert /usr/share/ca-certificates/CAcert/root.crt -sLi "https://icvpn1.chaosnetz.org:8989/pxe/hostname/${MAC}" 2> /dev/zero | tail -n1`
-rm -f /etc/hostname && touch /etc/hostname && echo "$HOSTNAME" >> /etc/hostname
-/etc/init.d/hostname.sh
+#HOSTNAME=`curl --cacert /usr/share/ca-certificates/CAcert/root.crt -sLi "https://icvpn1.chaosnetz.org:8989/pxe/hostname/${MAC}" 2> /dev/zero | tail -n1`
+curl --cacert /usr/share/ca-certificates/CAcert/root.crt -sLi "https://icvpn1.chaosnetz.org:8989/pxe/hostname/${MAC}" | tail -n1
+#rm -f /etc/hostname && touch /etc/hostname && echo "$HOSTNAME" >> /etc/hostname
+#/etc/init.d/hostname.sh
+
