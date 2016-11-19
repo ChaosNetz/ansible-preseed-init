80153e1d95757ac343c4ec9eef450232b65c87bb
Kai Oliver Quambusch <kq@qsc.de>
2016-11-19 18:46:51 +0100

    fixed inventur.sh

--- roles/c4-inventur/files/inventur.sh
+++ roles/c4-inventur/files/inventur.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 export LC_ALL=C
 
-fn="`date +%s`"
-lshw -json > /tmp/$fn
-sn="`python2 /usr/local/bin/serial.py /tmp/$fn`"
-mv /tmp/$fn /tmp/$sn-$fn
-curl -F "lshw=@/tmp/$sn-$fn" https://icvpn1.chaosnetz.org:8989/pxe/lshw/$(echo -ne `hostname`)
-
+curl --cacert /usr/share/ca-certificates/CAcert/root.crt -F "lshw=`lshw -json`" "https://icvpn1.chaosnetz.org:8989/pxe/lshw/$(hostname)"
--- roles/c4-inventur/files/serial.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python2
-from json import loads
-from sys import argv
-
-if __name__ == '__main__':
-    with open(argv[1]) as f:
-        j = loads(f.read())
-    print j["serial"]
