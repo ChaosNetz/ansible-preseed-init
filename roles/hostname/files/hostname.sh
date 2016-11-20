c8d7f4910bb41fcf03d69401459418a68a3535e3
yanosz <yanosz@gmx.net>
2016-11-20 11:06:55 +0100

    Hostname: Use chaosnetz in case of error

--- roles/hostname/files/hostname.sh
+++ roles/hostname/files/hostname.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 MAC=$(ip link show 2> /dev/null | tail -n 1 |  egrep -oi 'ether ([a-f0-9]{2}:){5}[a-f0-9]{2}' | cut --delimiter=' ' -f2 | tr ':' '-')
-curl --cacert /usr/share/ca-certificates/CAcert/root.crt -sLi "https://icvpn1.chaosnetz.org:8989/pxe/hostname/${MAC}" | tail -n1
-
+gnerated_hostname=$(curl -f --cacert /usr/share/ca-certificates/CAcert/root.crt -sLi "https://icvpn1.chaosnetz.org:8989/pxe/hostname/${MAC}" | tail -n1)
+if [ -z $generated_hostname ]; then
+  generated_hostname="chaosnetz"
+fi
+echo $generated_hostname
