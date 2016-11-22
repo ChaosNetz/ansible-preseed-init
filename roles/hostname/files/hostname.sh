0a544b24e94cc892d8269f7a51e0548aec4aa352
Kai Oliver Quambusch <kq@qsc.de>
2016-11-22 21:12:33 +0100

    fixed typo in generated_hostname

--- roles/hostname/files/hostname.sh
+++ roles/hostname/files/hostname.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 MAC=$(ip link show 2> /dev/null | tail -n 1 |  egrep -oi 'ether ([a-f0-9]{2}:){5}[a-f0-9]{2}' | cut --delimiter=' ' -f2 | tr ':' '-')
-gnerated_hostname=$(curl -f --cacert /usr/share/ca-certificates/CAcert/root.crt -sL "https://icvpn1.chaosnetz.org:8989/pxe/hostname/${MAC}")
+generated_hostname=$(curl -f --cacert /usr/share/ca-certificates/CAcert/root.crt -sL "https://icvpn1.chaosnetz.org:8989/pxe/hostname/${MAC}")
 if [ -z $generated_hostname ]; then
   generated_hostname="chaosnetz"
 fi
