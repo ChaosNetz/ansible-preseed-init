1432937854b916cfabd3a56eb758f2436eb5b628
yanosz <github@yanosz.net>
2017-10-22 20:19:01 +0200

    Fix missing space

--- roles/c4-inventur/files/inventur.sh
+++ roles/c4-inventur/files/inventur.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export LC_ALL=C
 
-curl -f-sLi -X "POST" -d @- "https://icvpn1.chaosnetz.org:8989/pxe/lshw/$(hostname)" << CURL_DATA
+curl -f -sLi -X "POST" -d @- "https://icvpn1.chaosnetz.org:8989/pxe/lshw/$(hostname)" << CURL_DATA
 `echo -ne "$(lshw -json)"`
 CURL_DATA
