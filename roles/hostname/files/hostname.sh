80944fe24498508c3a4164e841d8eb4e6471fd69
yanosz <github@yanosz.net>
2017-10-22 13:35:00 +0200

    Fixed Hostname / Cert

--- roles/hostname/files/hostname.sh
+++ roles/hostname/files/hostname.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 MAC=$(ip link show 2> /dev/null | tail -n 1 |  egrep -oi 'ether ([a-f0-9]{2}:){5}[a-f0-9]{2}' | cut --delimiter=' ' -f2 | tr ':' '-')
-generated_hostname=$(curl -f --cacert /usr/share/ca-certificates/CAcert/root.crt -sL "https://icvpn1.chaosnetz.org:8989/pxe/hostname/${MAC}")
+generated_hostname=$(curl -sL "https://icvpn1.chaosnetz.org:8989/pxe/hostname/${MAC}")
 if [ -z $generated_hostname ]; then
   generated_hostname="chaosnetz"
 fi
 echo $generated_hostname
+
